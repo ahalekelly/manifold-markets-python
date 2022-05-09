00e51a410b83baf564ed6f4a32e080684c076a3b
vluzko <vincent.luczkow@gmail.com>
2022-05-09 12:06:41 -0400

    Metaforecast manifold endpoint. Missing historical probabilities though

--- manifold/metaforecast_api.py
+++ manifold/metaforecast_api.py
@@ -1,29 +1,49 @@
-graphQLendpoint = "https://metaforecast.org/api/graphql";
+from distutils.command.build import build
+from typing import Optional
+import requests
 
-def build_query(end_cursor):
-    return """{
-    questions(first: 1000 {!!end_Cursor ? `after: "${endCursor}"` : ""}) {
-        edges {
-        node {
+
+GRAPH_QL_ENDPOINT = "https://metaforecast.org/api/graphql"
+
+
+def build_query(end_cursor: Optional[str]=None):
+    end_str = f"after: \"{end_cursor}\"" if end_cursor is not None else ""
+    return """{{
+    questions(first: 1000 {end_str}) {{
+        edges {{
+        node {{
             id
             title
             url
             description
-            options {
+            options {{
             name
             probability
-            }
-            qualityIndicators {
+            }}
+            qualityIndicators {{
             numForecasts
             stars
-            }
+            }}
             timestamp
-        }
-        }
-        pageInfo {
+        }}
+        }}
+        pageInfo {{
         endCursor
         startCursor
-        }
-    }
-    }""".format(end_cursor=end_cursor)
+        }}
+    }}
+    }}""".format(end_str=end_str)
+
+
+def get_all_questions():
+    all_results = []
+    first_result = requests.post(GRAPH_QL_ENDPOINT, json={"query": build_query(None)}).json()
+    end_cursor = first_result['data']['questions']['pageInfo']['endCursor']
+    all_results.extend([x['node'] for x in first_result['data']['questions']['edges']])
+    while end_cursor:
+        new_res = requests.post(GRAPH_QL_ENDPOINT, json={"query": build_query(end_cursor)}).json()
+        end_cursor = new_res['data']['questions']['pageInfo']['endCursor']
+        all_results.extend([x['node'] for x in new_res['data']['questions']['edges']])
+    manifold = [x for x in all_results if x['id'].split('-')[0] == 'manifold']
+    return manifold
 
